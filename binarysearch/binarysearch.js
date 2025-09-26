865a7aa1538c759971431e7edd86305aa4945976
Gideon Abe <gideonabe2020@gmail.com>
2025-09-26 18:53:18 +0100

    timer

--- binarysearch/binarysearch.js
+++ binarysearch/binarysearch.js
@@ -22,4 +22,8 @@ function binary_search(list, item){
 
 my_list = [1, 3, 5, 7, 9, 10]
 
+console.time("BinarySearch")
+const test = binary_search(my_list, 10)
+console.timeEnd("BinarySearch")
+
 console.log(binary_search(my_list, 10))
